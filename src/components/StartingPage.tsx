3c562cf5838471e511b7f94fb49042186037d39c
Milin7 <isaac_martinez.g@hotmail.com>
2025-04-24 07:23:53 -0600

    test if github account is working

--- src/components/StartingPage.tsx
+++ src/components/StartingPage.tsx
@@ -7,7 +7,7 @@ function StartingPage() {
       <div className=" mt-10 mr-10">
         <div className="border-2 text-white border-landing-neon rounded-xl p-7">
           <h1 className=" uppercase text-landing-neon tracking-widest  text-2xl mt-2 ">
-            Experience
+            From milin github on Tovis mac
           </h1>
           {jobs.map((jobList) => (
             <ExperienceCard jobList={jobList} key={jobList.id} />
